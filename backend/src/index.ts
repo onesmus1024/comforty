bfa9389b1a6cb658806db4ca1559c0f218dc4cb4
Kevin Malomba <malombakevin@gmail.com>
2023-02-14 20:31:36 +0300

    feat;middlewares - Adds middlewares

---
+++ backend/src/index.ts
@@ -0,0 +1,49 @@
+import express, { Express } from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import cors from 'cors';
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+
+import router from './Routers/user.router.js';
+import productRouter from './Routers/product.router.js';
+import categoryRouter from './Routers/category.router.js';
+import orderRouter from './Routers/order.router.js';
+import orderItemRouter from './Routers/orderItem.router.js';
+import paymentRouter from './Routers/payment.router.js';
+import paymentMethodRouter from './Routers/paymentMethod.router.js';
+import reviewRouter from './Routers/review.router.js';
+import cartItemRouter from './Routers/cartItem.route.js';
+const PORT = process.env.PORT || 6000;
+const app: Express = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use('/api/users', router);
+app.use('/api/products', productRouter);
+app.use('/api/categories', categoryRouter);
+app.use('/api/orders', orderRouter);
+app.use('/api/orderitems', orderItemRouter);
+app.use('/api/payments', paymentRouter);
+app.use('/api/paymentmethods', paymentMethodRouter);
+app.use('/api/reviews', reviewRouter);
+app.use('/api/cartitems', cartItemRouter);
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    
+    
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello!');   
+});
+
+
+
+
+
+
